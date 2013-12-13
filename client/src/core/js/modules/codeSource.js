20fe5ce6eb584a3d28c90c4770a8eb73beee5272
denisprzendzinskis <deniss.przendzinskis@odnoklassniki.ru>
2013-12-13 10:18:06 +0200

    New syntax highlighter (prism.js) added, old one (SyntaxHighlighter) removed. New html formatter (codeFormat) added. Changes in codeSource to adopt new plugins. Minor changes in css and less files related to new plugins.
    Squashed commit of the following:
    
    commit 0c2ac17677d13b68ae428e8e7ad07eab53da8a99
    Author: denisprzendzinskis <deniss.przendzinskis@odnoklassniki.ru>
    Date:   Fri Dec 13 09:53:14 2013 +0200
    
        Changes according to review
    
    commit 4f437335e655978bda3cf4c136d6f921180bef2c
    Author: denisprzendzinskis <deniss.przendzinskis@odnoklassniki.ru>
    Date:   Thu Dec 12 11:49:22 2013 +0200
    
        Changes according to review
    
    commit 025a017e246efb94468eb49625ae448195a698ae
    Author: denisprzendzinskis <deniss.przendzinskis@odnoklassniki.ru>
    Date:   Thu Dec 12 11:42:03 2013 +0200
    
        Changes according to review
    
    commit e87f6de13102b047ab23f1704e9b23461ddf317e
    Author: denisprzendzinskis <deniss.przendzinskis@odnoklassniki.ru>
    Date:   Wed Dec 11 10:51:30 2013 +0200
    
        Changes according to review
    
    commit d878597aaead391f6117bd41597b1d76efcc35aa
    Author: denisprzendzinskis <deniss.przendzinskis@odnoklassniki.ru>
    Date:   Mon Dec 9 14:31:02 2013 +0200
    
        formatify plugin for HTML code, new syntax highlighter (prism.js) and corresponding changes in codeSource

--- client/src/core/js/modules/codeSource.js
+++ client/src/core/js/modules/codeSource.js
@@ -1,10 +1,10 @@
 /*
-*
-* View demo-examples source code
-*
-* @author Robert Haritonov (http://rhr.me)
-*
-* */
+ *
+ * View demo-examples source code
+ *
+ * @author Robert Haritonov (http://rhr.me)
+ *
+ * */
 
 define([
     "jquery",
@@ -12,19 +12,18 @@ define([
     "modules/utils",
     "modules/css",
     "modules/browser",
-    "core/lib/SyntaxHighlighter/XRegExp",
-    "modules/innerNavigation"
-    ], function($, options, utils, css, browser, sh, innerNavigation) {
-
+    "core/lib/codeFormat",
+    "modules/innerNavigation",
+    "core/lib/prism/prism"
+], function($, options, utils, css, browser, codeFormat, innerNavigation, prism) {
     if (!($.browser.msie && parseInt($.browser.version) < 9)) { // and if not ie < 9
-
-        $(function(){
-
+        $(document).ready(function() {
             var
 
                 SourceCode = 'source_source-code',
                 SourceCodeShow = SourceCode + '__show',
                 SourceCodeMin = SourceCode + '__min',
+                SourceCodeStatic = SourceCode + '__static',
 
                 SourceCodeCnt = 'source_source-code_cnt',
 
@@ -36,101 +35,111 @@ define([
                 SourceCodeToggleAll = 'source_source-code_toggle-all',
                 SourceCodeToggleAllHide = SourceCodeToggleAll + '__hide',
 
-                RES_HIDE_CODE = 'Show',
-                RES_SHOW_CODE = 'Hide',
+                RES_HIDE_CODE = 'Hide',
+                RES_SHOW_CODE = 'Show',
                 RES_CODE = 'code',
                 RES_TOGGLER_SHOW_CODE = 'Show source',
 
                 urlConfig = 'sc',
 
                 prepared = false,
-                highlighted = false;
+                onlyStatic = true;
 
-            //Add HTML code container before each example without it by default
-            var createAndFillBrushHTML = function() {
 
-                $('.source_example').each(function () {
-                    var t = $(this);
+            //Add HTML source code container before each example without it
+            $('.source_example').each(function () {
+                var _this = $(this);
+                // <pre><code>...</code></pre> is the wrapper for prism.js plugin
+                if (!_this.prev().is('pre')) {
+                    _this.before('<pre class="language-markup line-numbers" style="display:none"><code class="language-markup"></code></pre>')
+                }
+            });
 
-                    //script type="text/html" used to isolate repeated HTML
-                    if (!t.prev().hasClass('html')) {
-                        t.before('<code class="brush : html"><script class="source_example_seal" type="text/html"></script></code>')
+            var wrapOldTags = function() {
+                $('.source_section').find('code[class*="brush"]').each(function () {
+                    var _this = $(this)
+                        ,formatClass = ' language-';
+
+                    if (_this.hasClass('css')) {
+                        formatClass += 'css';
+                    } else if (_this.hasClass('html')) {
+                        formatClass += 'markup';
+                    } else if (_this.hasClass('js')) {
+                        formatClass += 'javascript';
+                    }
+
+                    if (_this.hasClass('source_visible')) {
+                        formatClass += ' source_visible';
+                    }
+
+                    _this.removeClass().addClass(formatClass);
+
+                    if (!_this.parent().is('pre')) {
+                        _this.html().trim();
+                        _this.wrap('<pre style="display:none" class="line-numbers'+formatClass+'"></pre>');
                     }
                 });
-
-                //Auto copy HTML in code.html if it's now filled
-                $('.source_section > code.html > script').each(function () {
-                    if ($(this).html().trim().length === 0) {
-                        var HTMLcode = $(this).parent().nextAll('.'+ options.exampleSectionClass ).html();
-
-                        $(this).html(HTMLcode);
-                    }
-                });
-            };
-
-            //Code show toggle on each code blcock
+            }
+            //Code show toggle on each code block
             var prepareCodeBlocks = function() {
+                new css('prism/prism.css','/core/js/lib/');
+                var selection = (onlyStatic ? $('.source_section > pre.source_visible > code') : $('.source_section > pre > code'));
+                selection.each(function () {
+                    var _this = $(this)
+                        ,langClass='';
 
-                new css('SyntaxHighlighter/shCoreDefault.css','/core/js/lib/');
-                new css('SyntaxHighlighter/shCoreCustom.css','/core/js/lib/');
-
-                $('.source_section > code.brush:not(.source_visible)').each(function () {
-                    var t = $(this),
-                            langClass;
-
-                    if (t.hasClass('css')) {
+                    if (_this.hasClass('language-css')) {
                         langClass = SourceCodeToggleCSS;
-                    } else if (t.hasClass('html')) {
+                    } else if (_this.hasClass('language-markup')) {
                         langClass = SourceCodeToggleHTML;
-                    } else if (t.hasClass('js')) {
+                    } else if (_this.hasClass('language-javascript')) {
                         langClass = SourceCodeToggleJS;
                     }
 
-                    t.wrap('<div class="source_source-code"><div class="' + SourceCodeCnt + '"></div></div>');
-
-                    t.closest('.' + SourceCode).prepend('' +
-                            '<a href="" onclick="return false" class="' + SourceCodeToggle + ' ' + langClass + '"><span class="source_hide">' + RES_HIDE_CODE + '</span><span class="source_show">' + RES_SHOW_CODE + '</span> ' + RES_CODE + '</a>' +
-                            '');
+                    if (_this.parent().hasClass('source_visible')) {
+                        _this.parent().wrap('<div class="'+SourceCode+' '+SourceCodeStatic+'"><div class="' + SourceCodeCnt + '"></div></div>');
+                    }
+                    else {
+                        _this.parent().wrap('<div class="'+SourceCode+'"><div class="' + SourceCodeCnt + '"></div></div>');
+                        _this.closest('.' + SourceCode).prepend('' +
+                        '<a href="" class="' + SourceCodeToggle + ' ' + langClass + '"><span class="source_hide">' + RES_HIDE_CODE + '</span><span class="source_show">' + RES_SHOW_CODE + '</span> ' + RES_CODE + '</a>' +
+                        '');
+                    }
 
                 });
             };
+            var fillCodeContainers = function() {
+                //Auto copy HTML in code.html if it's now filled
+                var selection = (onlyStatic ? $('.source_section > pre.source_visible > code') : $('.source_section > pre > code'));
+                selection.each(function () {
+                    var _this = $(this), HTMLcode;
 
-            var activateHighlighter = function(handler) {
-                //shCore need xRegExp to work
-                require(["jquery","core/lib/SyntaxHighlighter/shCore"], function($) {
-
-                    //Unseal from script type=text/html
-                     $('code.brush > script.source_example_seal').each(function () {
-                         var source = $(this).html();
-                         $(this).parent().html(source);
-                     });
-
-                    SyntaxHighlighter.config.tagName = "code";
-                    SyntaxHighlighter.defaults['toolbar'] = false;
-                    SyntaxHighlighter.defaults['quick-code'] = false;
-                    SyntaxHighlighter.defaults['tab-size'] = 2;
-
-                    var highlighterLibs = [
-                        "core/lib/SyntaxHighlighter/shBrushCss",
-                        "core/lib/SyntaxHighlighter/shBrushXml",
-                        "core/lib/SyntaxHighlighter/shBrushSass",
-                        "core/lib/SyntaxHighlighter/shBrushJScript"
-                    ];
-
-                    require(highlighterLibs, function() {
-                        SyntaxHighlighter.highlight();
-                        highlighted = true;
-
-                        if(handler != undefined) {
-                            handler();
+                    if (_this.html().trim().length === 0) {
+                        HTMLcode = _this.parent().nextAll('.'+ options.exampleSectionClass ).html();
+                        _this.html(HTMLcode);
+                        if (_this.hasClass('language-markup')) {
+                            _this.formatify();
                         }
-                    })
-
-                })
+                    }
+                    else {
+                        HTMLcode = _this.html();
+                        console.log(HTMLcode);
+                        var spaces = HTMLcode.replace(/\t/,'  ').match(/^\s{0,}/)[0].length;
+                        var HTMLarray = HTMLcode.trim().split("\n");
+                        for (var i=0; i<HTMLarray.length;i++) {
+                            HTMLarray[i] = HTMLarray[i].replace(new RegExp(' {'+(spaces-1)+'}'), '');
+                        }
+                        HTMLcode = HTMLarray.join('\n').replace(/\s{1,}<span class="line-num/g, '<span class="line-num');
+                        if (_this.hasClass('language-markup'))
+                            HTMLcode = HTMLcode.replace(/<!--/g, "&lt;!--").replace(/-->/g,"--&gt;");
+                        _this.html(HTMLcode);
+                    }
+                });
             };
 
             var afterActivation = function() {
-                $('.' + SourceCode).addClass(SourceCodeShow);
+                var sources = $('.' + SourceCode);
+                sources.addClass(SourceCodeShow);
 
                 //Scroll to section
                 var navHash = utils.parseNavHash();
@@ -140,9 +149,9 @@ define([
             //Toggle show source sections
             var initCodePartToggler = function() {
                 $('.' + SourceCodeToggle).on({
-                    'click' : function () {
-                        var t = $(this),
-                                codeCnt = t.closest('.' + SourceCode);
+                    'click' : function (e) {
+                        e.preventDefault();
+                        var codeCnt = $(this).closest('.' + SourceCode);
 
                         if (codeCnt.hasClass(SourceCodeMin)) { //If section minimized
                             codeCnt.removeClass(SourceCodeMin);
@@ -153,54 +162,31 @@ define([
                 });
             };
 
-            //Get source code before triming
-            if (options.modulesEnabled.trimSpaces) {
-                createAndFillBrushHTML();
-            }
-
-            //temp for spec documentation, TODO: make possible to leave visible code blocks
-            if ( $('.source_section > code.brush.source_visible').length != 0 ) {
-                //If triming is off, then prepare code fill brushes only on activation
-                if (!options.modulesEnabled.trimSpaces) {
-                    createAndFillBrushHTML();
-                }
-
-                prepareCodeBlocks();
-                initCodePartToggler();
-
-                prepared = true;
-
-                activateHighlighter();
-            }
-
-
-            //Show/hide source
+            //Show / hide source
             var showAllCode = function () {
                 if (!prepared) {
-
-                    //If triming is off, then prepare code fill brushes only on activation
-                    if (!options.modulesEnabled.trimSpaces) {
-                        createAndFillBrushHTML();
-                    }
-
+                    fillCodeContainers();
                     prepareCodeBlocks();
+                    Prism.highlightAll();
                     initCodePartToggler();
-
+                    $('pre').removeAttr('style');
                     prepared = true;
                 }
+                afterActivation();
 
-                if (!highlighted) {
-                    activateHighlighter(function() {
-                        afterActivation();
-                    });
-                } else {
-                    afterActivation();
-                }
-
-            //TODO: finish url update on code show toggle functionality
+                //TODO: finish url update on code show toggle functionality
 //                var updateHref = hash === '' ? '!sc' : hash + urlConfig;
 //                window.location.hash = updateHref;
             };
+
+            var showStaticCode = function () {
+                wrapOldTags();
+                fillCodeContainers();
+                prepareCodeBlocks();
+                onlyStatic = false;
+                Prism.highlightAll();
+                $('pre.source_visible').removeAttr('style');
+            }
 
             var hideAllCode = function () {
                 $('.' + SourceCode).removeClass(SourceCodeShow);
@@ -211,18 +197,24 @@ define([
                 utils.scrollToSection(navHash);
             };
 
-            //If url has '!sc', show source code by default
+//            If url has '!sc', show source code by default
             var getUrlParam = document.location.href.split(options.modulesOptions.innerNavigation.hashSymb);
 
             getUrlParam = getUrlParam[getUrlParam.length - 1];
 
             if (getUrlParam === urlConfig) {
-                showAllCode();
+                $(document).ajaxStop(function() {
+                    showAllCode();
+                    $('.source_main_nav_ac_tx:contains("'+RES_TOGGLER_SHOW_CODE+'")').next('.source_slider_frame').trigger('click');
+                });
             }
 
-            //Toggle show all code action
-            innerNavigation.addMenuItem(RES_TOGGLER_SHOW_CODE, showAllCode, hideAllCode);
+            //Add Toggle show all code action if required
+            if ($('pre:not(.source_visible)')[0]) {
+                innerNavigation.addMenuItem(RES_TOGGLER_SHOW_CODE, showAllCode, hideAllCode);
+            }
 
+            showStaticCode();
         });
     }
 });
